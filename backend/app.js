0294f6a6ade52ee3ff54b3d4a099d8e56485018a
Nikhil Koushik <118748745+Nikhil-Koushik@users.noreply.github.com>
2023-08-26 15:19:36 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const _ = require("lodash");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname, '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.set("view engine","ejs");
 app.set('trust proxy', 1);
 
