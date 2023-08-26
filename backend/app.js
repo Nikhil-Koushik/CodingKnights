331a87ff6e10a9af80e8d275a97437c5ba4902de
Nikhil-Koushik <118748745+Nikhil-Koushik@users.noreply.github.com>
2023-08-26 13:44:11 +0530

    changed

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 mongoose.connect(process.env.MONGO,{useNewUrlParser:true,useUnifiedTopology:true});
+// mongoose.connect("",{useNewUrlParser:true,useUnifiedTopology:true});
 
 const userSchema = new mongoose.Schema({
   email: String,
