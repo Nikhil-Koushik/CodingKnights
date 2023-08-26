3fbd177df8a381091b6dd8f9c6949830c43613af
Nikhil Koushik <118748745+Nikhil-Koushik@users.noreply.github.com>
2023-08-26 14:10:09 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const session = require("express-session");
+const session = require("cookie-session");
 const passport = require("passport");
 const findOrCreate = require("mongoose-findorcreate");
 const passportLocalMongoose = require("passport-local-mongoose");
