6756aa27d52b7550e11f0110064d33e09d5048d8
Nikhil-Koushik <Nikhil-Koushik@users.noreply.github.com>
2026-02-04 06:43:24 +0530

    fix: fixes login issue

--- Run/app.js
+++ Run/app.js
@@ -9,59 +9,58 @@ const passport = require("passport");
 const findOrCreate = require("mongoose-findorcreate");
 const passportLocalMongoose = require("passport-local-mongoose");
 const _ = require("lodash");
+
 const app = express();
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, '/public')));
 app.set("view engine","ejs");
 app.set('trust proxy', 1);
 
-const MemoryStore = require('memorystore')(session)
+const MemoryStore = require('memorystore')(session);
 
 app.use(session({
     cookie: { maxAge: 60000 },
     store: new MemoryStore({
-      checkPeriod: 60000 // prune expired entries every 24h
+      checkPeriod: 60000
     }),
     resave: false,
-    secret: 'keyboard cat'
-}))
+    saveUninitialized: false,
+    secret: process.env.SECRET
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 mongoose.connect(process.env.MONGO,{useNewUrlParser:true,useUnifiedTopology:true});
-// mongoose.connect("",{useNewUrlParser:true,useUnifiedTopology:true});
+
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String
-})
+});
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
-const User = new mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, cb) {
-  process.nextTick(function() {
-    return cb(null, user.id);
-  });
-});
+passport.serializeUser((user, cb) => cb(null, user.id));
 
-passport.deserializeUser(function(id, cb) {
+passport.deserializeUser((id, cb) => {
   User.findById(id)
-  .then(function(user)
-    {
-      return cb(null, user);
-    })
-  .catch(function(err)
-    {
-      return cb(err);
-    })
-
+    .then(user => cb(null, user))
+    .catch(err => cb(err));
 });
+
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
+}
 
 
 const batchSchema = {
@@ -70,190 +69,185 @@ const batchSchema = {
       batchName : String,
       batchGetter: String,
       batchDays: {
-                    dates: [
-                              {
-                                date: String,
-                                title: String,
-                                content: String,
-                                zoom: String,
-                                doc: String,
-                                comments: [{User:String,Comment:String}]
-                              }
-                          ]
-                  }
+        dates: [
+          {
+            date: String,
+            title: String,
+            content: String,
+            zoom: String,
+            doc: String,
+            comments: [{User:String,Comment:String}]
+          }
+        ]
+      }
     }
   ]
-}
+};
 
-const Batch = mongoose.model("Batch",batchSchema);
+const Batch = mongoose.model("Batch", batchSchema);
 
-app.get("/",function(req,res){
+
+app.get("/", (req,res)=>{
   res.render("login",{msg:""});
-})
+});
 
-app.get("/"+process.env.REGISTER,function(req,res){
+app.get("/"+process.env.REGISTER, (req,res)=>{
   res.render("register");
-})
-app.post("/"+process.env.REGISTER,function(req,res){
-  User.register({username: req.body.username},req.body.password,function(err,user){
-    if (err){
-      res.redirect("/registerNikhilRightNow");
-    } else {
-      passport.authenticate("local")(req,res,function(){
-        res.redirect("/home");
-      });
-    }
-  });
-})
-app.get("/home",function(req,res){
-  if (req.isAuthenticated()){
-    res.render("home",{un:req.user.username});
-  } else {
-    res.redirect("/login");
-  }
-});
-app.get("/login",function(req,res){
-  if (req.isAuthenticated()){
-    res.render("home",{un:req.user.username});
-  } else {
-    res.render("login",{msg:""});
-  }
 });
 
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/home', failWithError: true }),
-  function(err, req, res, next) {
-    return res.render('login',{msg:"Invalid User"});
+app.post("/"+process.env.REGISTER, (req,res)=>{
+  User.register({username: req.body.username}, req.body.password, (err,user)=>{
+    if (err) return res.redirect("/registerNikhilRightNow");
+
+    passport.authenticate("local")(req,res,()=>{
+      res.redirect("/home");
+    });
+  });
+});
+
+app.get("/login", (req,res)=>{
+  if (req.isAuthenticated()){
+    return res.redirect("/home");
   }
+  res.render("login",{msg:""});
+});
+
+app.post("/login",
+  passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login'
+  })
 );
 
-app.get("/beginner",function(req,res){
-  Batch.find().then(function(batches){
-    batches.forEach((item, i) => {
-      res.render("beginner",{un:req.user.username,arr:item.batchArray});
-    });
-    }).catch(function(err){console.log(err);});
-  });
-
-
-app.get("/beginner/:batch",function(req,res){
-  Batch.find().then(function(batches){
-    batches.forEach((item, i) => {
-      const data = item.batchArray;
-      data.forEach((batch, i) => {
-        if (batch.batchGetter === req.params.batch){
-          res.render("batches",{un:req.user.username,data:batch})
-        }
-      });
-    });
-  }).catch(function(err){console.log(err);});
+app.get("/home", isLoggedIn, (req,res)=>{
+  res.render("home",{un:req.user.username});
 });
 
-app.get("/beginner/:batch/:day",function(req,res){
-  Batch.find().then(function(batches){
-    batches.forEach((item, i) => {
-      const data = item.batchArray;
-      data.forEach((batch, i) => {
-        if (batch.batchGetter === req.params.batch){
-          batch.batchDays.dates.forEach((specificDate, i) => {
-            if (specificDate.date === req.params.day){
-              console.log(specificDate);
-              res.render("days",{un:req.user.username,batch:req.params.batch,day:req.params.day,data:specificDate})
-            }
-          });
 
-        }
-      });
-
+app.get("/beginner", isLoggedIn, async (req,res)=>{
+  try {
+    const batchDoc = await Batch.findOne();
+    res.render("beginner",{
+      un:req.user.username,
+      arr: batchDoc?.batchArray || []
     });
-
-  }).catch(function(err){console.log(err);});
-});
-app.post("/beginner/:batch/:day",function(req,res){
-  const cmnt = {User:req.user.username,Comment: req.body.comment};
-  Batch.find().then(function(batches){
-    batches.forEach((item, i) => {
-      const data = item.batchArray;
-      data.forEach((batch, j) => {
-        if (batch.batchGetter === req.params.batch){
-          batch.batchDays.dates.forEach((specificDate, k) => {
-            if (specificDate.date === req.params.day){
-              batches[i].batchArray[j].batchDays.dates[k].comments.push(cmnt);
-              console.log(batches[i].batchArray[j].batchDays.dates[k]);
-              batches[i].save();
-              res.redirect("/beginner/"+req.params.batch+"/"+req.params.day);
-
-            }
-          });
-
-        }
-      });
-
-    });
-
-  }).catch(function(err){console.log(err);});
-  // Batch.findOne({batchGetter: req.params.batch}).then(
-  //   function(foundList){
-  //     foundList.batchDays.dates.forEach((item, i) => {
-  //       if (item.date === req.params.day){
-  //         // console.log(req);
-  //         foundList.batchDays.dates[i].comments.push({"User":req.user.username,"Comment":cmnt});
-  //         foundList.save();
-  //         res.redirect("/beginner/"+req.params.batch+"/"+req.params.day);
-  //       }
-  //     });
-  //   }
-  // ).catch(function(err){console.log(err);});
-});
-app.get("/logout",function(req,res){
-  req.logout(function(err){
+  } catch (err){
     console.log(err);
-  });
-  res.redirect("/");
+    res.sendStatus(500);
+  }
 });
 
-app.get("/aboutUs",function(req,res){
+app.get("/beginner/:batch", isLoggedIn, async (req,res)=>{
+  try {
+    const batchDoc = await Batch.findOne({
+      "batchArray.batchGetter": req.params.batch
+    });
+
+    if (!batchDoc) return res.sendStatus(404);
+
+    const batch = batchDoc.batchArray.find(
+      b => b.batchGetter === req.params.batch
+    );
+
+    res.render("batches",{
+      un:req.user.username,
+      data: batch
+    });
+
+  } catch (err){
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.get("/beginner/:batch/:day", isLoggedIn, async (req,res)=>{
+  try {
+    const batchDoc = await Batch.findOne({
+      "batchArray.batchGetter": req.params.batch
+    });
+
+    if (!batchDoc) return res.sendStatus(404);
+
+    const batch = batchDoc.batchArray.find(
+      b => b.batchGetter === req.params.batch
+    );
+
+    const day = batch.batchDays.dates.find(
+      d => d.date === req.params.day
+    );
+
+    if (!day) return res.sendStatus(404);
+
+    res.render("days",{
+      un:req.user.username,
+      batch:req.params.batch,
+      day:req.params.day,
+      data: day
+    });
+
+  } catch (err){
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/beginner/:batch/:day", isLoggedIn, async (req,res)=>{
+  try {
+    const cmnt = {
+      User:req.user.username,
+      Comment:req.body.comment
+    };
+
+    const result = await Batch.updateOne(
+      {
+        "batchArray.batchGetter": req.params.batch,
+        "batchArray.batchDays.dates.date": req.params.day
+      },
+      {
+        $push: {
+          "batchArray.$[].batchDays.dates.$[d].comments": cmnt
+        }
+      },
+      {
+        arrayFilters:[{ "d.date": req.params.day }]
+      }
+    );
+
+    if (!result.modifiedCount) return res.sendStatus(404);
+
+    res.redirect(`/beginner/${req.params.batch}/${req.params.day}`);
+
+  } catch (err){
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+
+app.get("/aboutUs", isLoggedIn, (req,res)=>{
   res.render("aboutUs",{un:req.user.username});
-})
+});
 
-app.get("/contactUs",function(req,res){
+app.get("/contactUs", isLoggedIn, (req,res)=>{
   res.render("contactUs",{un:req.user.username});
-})
+});
 
-app.get("/beginner/:batch/:day/zoom/:zoom",function(req,res){
-  res.status(301).redirect("https://us05web.zoom.us/j/"+req.params.zoom);
-})
-app.get("/beginner/:batch/:day/doc/:doc",function(req,res){
-  res.status(301).redirect("https://drive.google.com/file/d/"+req.params.doc+"/view?usp=sharing/");
-})
+app.get("/logout", (req,res,next)=>{
+  req.logout(err=>{
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
+
+app.get("/beginner/:batch/:day/zoom/:zoom", isLoggedIn, (req,res)=>{
+  res.redirect("https://us05web.zoom.us/j/"+req.params.zoom);
+});
+
+app.get("/beginner/:batch/:day/doc/:doc", isLoggedIn, (req,res)=>{
+  res.redirect("https://drive.google.com/file/d/"+req.params.doc+"/view?usp=sharing/");
+});
 
 
-
-app.listen(3000);
-
-
-
-
-// const l = ['Batch-1'];
-// l.forEach((item) => {
-//   const batch = new Batch({
-//     batchName: item,
-//     batchGetter: _.lowerFirst(item),
-//     batchDays: {
-//     dates: [
-//       {
-//         "date" : "Day-1",
-//           "title" : "Walking through",
-//           "content" : "Teaching",
-//           "zoom": "",
-//           "doc":"",
-//           "comments" : [
-//               "Miami",
-//               "Jhonas"
-//             ]
-//       }
-//     ]
-//     }
-//   });
-//   batch.save();
-// });
+app.listen(3000, ()=>{
+  console.log("Server running on port 3000");
+});
